fb1d425132b214412a383983125225cb813e465b
Tiep M. H <50709837+TiepMH@users.noreply.github.com>
2021-12-09 23:03:03 +0000

    Add files via upload

---
+++ C__VectorVector_Mult.m
@@ -0,0 +1,40 @@
+N = 17;
+x = randn(N, 1) + 1j*randn(N, 1);
+y = randn(N, 1) + 1j*randn(N, 1);
+
+xH_y_real = zeros(1);
+xH_y_imag = zeros(1);
+
+count_Multiplications = 0;
+count_Summations = 0;
+
+for n=1:N
+    if n==1
+        xH_y_real = real(x(n))*real(y(n)) + imag(x(n))*imag(y(n));
+        xH_y_imag = real(x(n))*imag(y(n)) - imag(x(n))*real(y(n));
+        count_Multiplications = count_Multiplications + 4;
+        count_Summations = count_Summations + 2;
+    else
+        xH_y_real = xH_y_real + real(x(n))*real(y(n)) + imag(x(n))*imag(y(n));
+        xH_y_imag = xH_y_imag + real(x(n))*imag(y(n)) - imag(x(n))*real(y(n));
+        count_Multiplications = count_Multiplications + 4;
+        count_Summations = count_Summations + 4;
+    end
+end
+
+%% We have to check if z_ = z
+z_ = x' *y;
+z = xH_y_real + 1j .* xH_y_imag;
+error = round(sum(z_ - z), 1);
+error
+
+%% Check if the number of FLOPS counted through simulation maches up with that derived theoretically
+
+FLOPS_Multiplications_theory = 4*N
+FLOPS_Summations_theory = 4*N-2
+
+count_Multiplications
+count_Summations
+
+
+
---
+++ D__MatrixVector_Mult.m
@@ -0,0 +1,42 @@
+M = 8; N = 17;
+A = randn(M, N) + 1j*randn(M, N);
+v = randn(N, 1) + 1j*randn(N, 1);
+
+Av_real = zeros(M, 1);
+Av_imag = zeros(M, 1);
+
+count_Multiplications = 0;
+count_Summations = 0;
+
+for m=1:M
+    for n=1:N
+        if n==1
+            Av_real(m) = real(A(m, n))*real(v(n)) - imag(A(m,n))*imag(v(n));
+            Av_imag(m) = real(A(m, n))*imag(v(n)) + imag(A(m,n))*real(v(n));
+            count_Multiplications = count_Multiplications + 4;
+            count_Summations = count_Summations + 2;
+        else
+            Av_real(m) = Av_real(m) + real(A(m, n))*real(v(n)) - imag(A(m,n))*imag(v(n));
+            Av_imag(m) = Av_imag(m) + real(A(m, n))*imag(v(n)) + imag(A(m,n))*real(v(n));
+            count_Multiplications = count_Multiplications + 4;
+            count_Summations = count_Summations + 4;
+        end
+    end
+end
+
+%% We have to check if z_ = z
+z_ = A*v;
+z = Av_real + 1j .* Av_imag;
+error = round(sum(z_ - z), 1);
+error
+
+%% Check if the number of FLOPS counted through simulation maches up with that derived theoretically
+
+FLOPS_Multiplications_theory = 4*M*N
+FLOPS_Summations_theory = M*(4*N-2)
+
+count_Multiplications
+count_Summations
+
+
+
